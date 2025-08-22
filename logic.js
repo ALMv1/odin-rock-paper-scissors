c7bb1fb193a69676c1805530fdb0391cd3759cad
ALMv1 <70581577+ALMv1@users.noreply.github.com>
2025-08-22 16:51:33 +0200

    add UI

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 
 let humanScore = 0;
 let computerScore = 0;
-
+let roundCounter = 0;
 
 function getComputerChoice(){
     let ran = Math.floor(Math.random() * 3)
@@ -20,41 +20,72 @@ function getComputerChoice(){
     return computerChoice
 }
 
-function getHumanChoice() {
-    const humanChoice = prompt("Rock, Paper or Scissors? ");
-    return humanChoice.toLowerCase();
-}   
-
 function playRound(humanChoice, computerChoice){
 
-    console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}!`);
+    const matchEnd = document.querySelector("#matchEnd");
+    if (roundCounter === 0){
+        matchEnd.textContent = "";
+    }
+    
+    const log = document.querySelector("#roundLog");
+    log.textContent = `You chose ${humanChoice} and the computer chose ${computerChoice}!`;
 
+    const result = document.querySelector("#result");
+    
     if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
-        console.log("You win this round!");
+        result.textContent = "You win this round!";
         humanScore++;
     } else if ((humanChoice === "rock" && computerChoice === "rock") || (humanChoice === "paper" && computerChoice === "paper") || (humanChoice === "scissors" && computerChoice === "scissors")){
-        console.log("You tied.");
+        result.textContent = "You tied this round!";
     } else {
-        console.log("You lost!");
+        result.textContent = "You lost this round!";
         computerScore++;
     }
+    
+    const disphumanScore = document.querySelector("#humanScore");
+    const dispcomputerScore = document.querySelector("#computerScore");
+    
+    disphumanScore.textContent = `Your score: ${humanScore}`;
+    dispcomputerScore.textContent = `CPU score: ${computerScore}`;
+
+    roundCounter++;
+    
+    if (roundCounter === 5){
+        if (humanScore > computerScore){
+            matchEnd.textContent = "Congrats, You win!";
+
+        } else if (humanScore === computerScore){
+            matchEnd.textContent = "You tied!";
+        } else {
+            matchEnd.textContent = "You lost! Try again!";
+        }
+        humanScore = 0;
+        computerScore = 0;
+        roundCounter = 0;
+    }
+
 }
 
-for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-}
+const rock = document.querySelector("#rock")
+rock.addEventListener("click", () => playRound("rock", getComputerChoice()))
 
-console.log(`The final score is:`);
-console.log(`You: ${humanScore}. Computer: ${computerScore}.`);
-if (humanScore > computerScore) {
-    console.log ("Congrats, You win!");
-}
-else if (humanScore === computerScore) {
-    console.log ("You tied!");
-}
-else {
-    console.log("You lost! Try again!");
-}
+const paper = document.querySelector("#paper")
+paper.addEventListener("click", () => playRound("paper", getComputerChoice()))
+
+const scissors = document.querySelector("#scissors")
+scissors.addEventListener("click", () => playRound("scissors", getComputerChoice()))
+
+
+
+// console.log(`The final score is:`);
+// console.log(`You: ${humanScore}. Computer: ${computerScore}.`);
+// if (humanScore > computerScore) {
+//     console.log ("Congrats, You win!");
+// }
+// else if (humanScore === computerScore) {
+//     console.log ("You tied!");
+// }
+// else {
+//     console.log("You lost! Try again!");
+// }
 
